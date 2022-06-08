1e272b60d74b0753dd847da849968ad204d4de23
Jpeels2811 <jos.herrera@ducocuc.cl>
2022-06-08 01:00:47 -0400

    django full

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import main, paginaregister
+from .views import *
 
 
 urlpatterns = [
     path('', main,name="main"),
     path('PaginaRegister/',paginaregister,name="paginaregister"),
-    
-    ]
+    path('PaginaLogin/', paginalogin, name="paginalogin"),
+    path('formulario1/', formulario1, name="formulario1"),
+    path('noticia1111/', noticia1, name="noticia1"),
+    path('noticia2222/', noticia2, name="noticia2"),
+    path('noticia3333/', noticia3, name="noticia3"),
+    path('main logeado/', mainlogeado, name="mainlogeado"),
+    path('buscador/', buscador, name="buscador"),
+    path('subir noticia/', subirnoticia, name="subirnoticia"),
+    ]   
